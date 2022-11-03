b568c3955947fb6ad13469542e91c076fd1282c5
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2022-11-03 18:29:13 -0500

    setting up basic project

--- mfcc-benchmark/__main__.py
+++ mfcc-benchmark/__main__.py
@@ -1,4 +1,19 @@
 from pathlib import Path
+from .util import ogg_to_array
+import requests
 
-dead_presidents=Path(__file__).parent.parent / "data" / "Jfk_rice_university_we_choose_to_go_to_the_moon.ogg"
+data_path=Path(__file__).parent.parent / "data"
+dead_presidents=data_path / "Jfk_rice_university_we_choose_to_go_to_the_moon.ogg"
+#if data_path.exists():
+#    #data_path.mkdir()
+#    headers = {
+#    'User-Agent': 'My User Agent 1.0'
+#    }
+#    r=requests.get("https://upload.wikimedia.org/wikipedia/commons/5/50/Jfk_rice_university_we_choose_to_go_to_the_moon.ogg",headers=headers)
+#    print(r)
+
+dead_presidents=data_path / "Jfk_rice_university_we_choose_to_go_to_the_moon.ogg"
+data= ogg_to_array(dead_presidents)
+i =0
+single_channel=data[0]
 
